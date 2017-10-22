6fe0b22ffaff2e8ca119aec8017b8cf32fbdefec
Andrew T Martin III <atmartin93@live.com>
2017-10-22 17:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidaysTo(name) {
   console.log(`${name}!`)
   return "Happy holidays, you!"
 }
+function happyHolidayTo(holiday, name) {
+  console.log('${holiday},${name}!')
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown(holiday, days) {
+  console.log('${holiday},${days}!')
+  return "It's 20 days until Mother's Day!"
+}
